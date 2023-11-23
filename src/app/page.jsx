9c5d34a37dd3e5a3b3eb91bde9acb0e093f0c7ea
joseruiz010 <joruiz765@gmail.com>
2023-11-23 15:31:05 -0300

    key2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ const HomaPage = async () => {
   return (
     <div className='grid grid-cols-4'>
       {
-        tasks.map(t => <TaskCard task={t} key={t.id + t.title} />)
+        tasks.map((t, i) => <TaskCard task={t} key={t.id + i} />)
       }
     </div>
   )
